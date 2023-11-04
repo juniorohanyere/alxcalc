d40df338f66278ddb67da3e9e01658d47de6763c
Twin J <junohanyere@gmail.com>
2023-11-04 14:55:43 +0100

    main method

--- alxcalc.py
+++ alxcalc.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Main module for the alxcalc program.
+"""
 
 import sys
 import subprocess
@@ -7,9 +9,16 @@ from month_0 import month_0
 
 
 def main():
+    """Entry point.
+    """
+
+    if len(sys.argv) <= 2:
+        print("Usage: {} <option> <value>".format(sys.argv[0]))
+        sys.exit()
+
     subprocess.run('editor month_0/month_0', shell=True, text=True)
+
     m0 = month_0.Month_0(args=sys.argv)
-    m0.main()
 
 
 if __name__ == '__main__':
