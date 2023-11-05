3cda27af85e0fdc73dd03987b6935bb56ce1c5f5
Twin J <junohanyere@gmail.com>
2023-11-05 05:02:04 +0100

    new objects: Month, Project, Task, and Calc

--- alxcalc.py
+++ alxcalc.py
@@ -6,7 +6,7 @@ import sys
 import os
 import subprocess
 
-from calc import Parser
+from calc import Month, Project, Task, Calc
 
 
 def main():
@@ -17,6 +17,15 @@ def main():
         print("Usage: {} <option> <value>".format(sys.argv[0]))
         sys.exit()
 
+    if sys.argv[1] != "-M" and sys.argv[1] != "-m":
+        print("Error: Invalid option: {}".format(sys.argv[1]))
+        sys.exit()
+
+    if sys.argv[2].isdigit() is False:
+        print("Error: Invalid input value for option {}: {}".
+              format(sys.argv[1], sys.argv[2]))
+        sys.exit()
+
     filename = 'month_{}/month_{}'.format(sys.argv[2], sys.argv[2])
 
     try:
@@ -25,9 +34,13 @@ def main():
         print("DEBUGGER 4: File Not Found")
         sys.exit()
 
-    subprocess.run(f'editor {filename}', shell=True, text=True)
+    if sys.argv[1] == "-M":
+        subprocess.run(f'editor {filename}', shell=True, text=True)
 
-    m0 = Parser(filename=filename, args=sys.argv)
+    month = Month(filename=filename, args=sys.argv)
+    project = Project(filename=filename, args=sys.argv)
+    task = Task(filename, args=sys.argv)
+    calc = Calc(filename, args=sys.argv)
 
 
 if __name__ == '__main__':
